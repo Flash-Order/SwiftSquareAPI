6bf1ef14741d03e34d198c3cb087c9476c0fa426
Jon Lund <jon.lund@gmail.com>
2021-04-26 14:34:18 -0600

    List all the things needed for the app

---
+++ Sources/Notes.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jon Lund on 4/26/21.
+//
+
+/*
+
+
+Things needed by app:
+
+Order
+	OrderLineItem
+Merchant
+Currency
+ListCatalog()
+ListCatalogResponse
+CatalogObject
+ItemVariationLocation
+CatalogItem
+CatalogTax
+Address
+TerminalCheckout
+DeviceCode
+Money
+Tender
+TenderCardDetails
+CardPaymentDetails
+Payment
+Empty
+
+
+*/
