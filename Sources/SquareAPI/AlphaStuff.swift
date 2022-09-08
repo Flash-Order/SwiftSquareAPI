10e62b09def46f8065b1ac3ee92fa5bcb65bc7a6
Jon Lund <jon.lund@gmail.com>
2022-09-08 09:34:14 -0600

    added an alpha fulfillmentstate since we're getting something undocumented ("OPEN")

--- Sources/SquareAPI/AlphaStuff.swift
+++ Sources/SquareAPI/AlphaStuff.swift
@@ -14,3 +14,22 @@ public typealias InfoCode = String
 public typealias GANSource = GiftCardGANSource
 //public typealias Reason = GiftCardActivityAdjustDecrement
 public typealias Reason = String
+
+
+/// The current state of this fulfillment.
+public enum OrderFulfillmentState_ALPHA: String, Codable {
+	/// Indicates that the fulfillment has been proposed.
+	case PROPOSED
+	/// Indicates that the fulfillment has been reserved.
+	case RESERVED
+	/// Indicates that the fulfillment has been prepared.
+	case PREPARED
+	/// Indicates that the fulfillment was successfully completed.
+	case COMPLETED
+	/// Indicates that the fulfillment was canceled.
+	case CANCELED
+	/// Indicates that the fulfillment failed to be completed, but was not explicitly canceled.
+	case FAILED
+	/// ????
+	case OPEN
+}
